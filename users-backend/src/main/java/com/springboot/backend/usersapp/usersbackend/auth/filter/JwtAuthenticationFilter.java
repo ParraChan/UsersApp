5fb502d0b4dce5653d58b33109f5d92cee8b6693
becario3 <becario3@rufi.mx>
2025-06-20 12:57:40 -0600

    metodo succesful agregado

--- users-backend/src/main/java/com/springboot/backend/usersapp/usersbackend/auth/filter/JwtAuthenticationFilter.java
+++ users-backend/src/main/java/com/springboot/backend/usersapp/usersbackend/auth/filter/JwtAuthenticationFilter.java
@@ -2,6 +2,8 @@ package com.springboot.backend.usersapp.usersbackend.auth.filter;
 
 import java.io.IOException;
 
+import javax.crypto.SecretKey;
+
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
@@ -13,63 +15,62 @@ import com.fasterxml.jackson.databind.DatabindException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.springboot.backend.usersapp.usersbackend.entities.User;
 
+import io.jsonwebtoken.Jwts;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-
+import static com.springboot.backend.usersapp.usersbackend.auth.TokenJwtConfig.*;
 public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
-
 
     private AuthenticationManager authenticationManager;
 
-    
-
     public JwtAuthenticationFilter(AuthenticationManager authenticationManager) {
-        this.authenticationManager = authenticationManager ;
+        this.authenticationManager = authenticationManager;
     }
-
-
 
     @Override
     public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response)
             throws AuthenticationException {
 
-                String username = null;
-                String password = null;
+        String username = null;
+        String password = null;
 
-                try {
-                    User user = new ObjectMapper().readValue(request.getInputStream(), User.class);
-                    username= user.getUsername();
-                    password = user.getPassword();
-                } catch (StreamReadException e) {
-                    e.printStackTrace();
-                } catch (DatabindException e) {
-                    e.printStackTrace();
-                } catch (IOException e) { 
-                    e.printStackTrace();
-                }
-                UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(username, password);
-     return this.authenticationManager.authenticate(authenticationToken);
+        try {
+            User user = new ObjectMapper().readValue(request.getInputStream(), User.class);
+            username = user.getUsername();
+            password = user.getPassword();
+        } catch (StreamReadException e) {
+            e.printStackTrace();
+        } catch (DatabindException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(username,
+                password);
+        return this.authenticationManager.authenticate(authenticationToken);
     }
-
-
 
     @Override
     protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain,
             Authentication authResult) throws IOException, ServletException {
-        super.successfulAuthentication(request, response, chain, authResult);
+
+        org.springframework.security.core.userdetails.User user = (org.springframework.security.core.userdetails.User) authResult
+                .getPrincipal();
+        String username = user.getUsername();
+
+
+        String jws = Jwts.builder()
+        .subject(username)
+        .signWith(SECRET_KEY)
+        .compact();
     }
-
-
 
     @Override
     protected void unsuccessfulAuthentication(HttpServletRequest request, HttpServletResponse response,
             AuthenticationException failed) throws IOException, ServletException {
         super.unsuccessfulAuthentication(request, response, failed);
     }
-    
-
-
 
 }
