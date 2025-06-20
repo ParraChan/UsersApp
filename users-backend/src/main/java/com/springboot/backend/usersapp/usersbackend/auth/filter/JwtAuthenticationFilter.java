e6d882de90ab35eb6860434eb47dfa571957458b
becario3 <becario3@rufi.mx>
2025-06-20 12:30:59 -0600

    consigurando JWT authentication filter

--- users-backend/src/main/java/com/springboot/backend/usersapp/usersbackend/auth/filter/JwtAuthenticationFilter.java
+++ users-backend/src/main/java/com/springboot/backend/usersapp/usersbackend/auth/filter/JwtAuthenticationFilter.java
@@ -13,6 +13,8 @@ import com.fasterxml.jackson.databind.DatabindException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.springboot.backend.usersapp.usersbackend.entities.User;
 
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
@@ -49,6 +51,22 @@ public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilte
                 }
                 UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(username, password);
      return this.authenticationManager.authenticate(authenticationToken);
+    }
+
+
+
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain,
+            Authentication authResult) throws IOException, ServletException {
+        super.successfulAuthentication(request, response, chain, authResult);
+    }
+
+
+
+    @Override
+    protected void unsuccessfulAuthentication(HttpServletRequest request, HttpServletResponse response,
+            AuthenticationException failed) throws IOException, ServletException {
+        super.unsuccessfulAuthentication(request, response, failed);
     }
     
 
