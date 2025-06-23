93108e7f044a3d30b2e06d717deff63577ebb4cd
becario3 <becario3@rufi.mx>
2025-06-23 10:34:21 -0600

    almacenando credencialezs

--- src/app/components/user-app.component.ts
+++ src/app/components/user-app.component.ts
@@ -5,6 +5,7 @@ import Swal from 'sweetalert2';
 import { ActivatedRoute, Router, RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './navbar/navbar.component';
 import { SharingDataService } from '../services/sharing-data.service';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'user-app',
@@ -23,6 +24,7 @@ export class UserAppComponent implements OnInit {
     private router: Router,
     private sharingData: SharingDataService,
     private service: UserService,
+    private authService: AuthService,
     private route: ActivatedRoute  ) {
   }
 
@@ -44,6 +46,39 @@ export class UserAppComponent implements OnInit {
     this.addUser();
     this.removeUser();
     this.findUserById();
+    this.handlerLogin();
+
+  }
+  handlerLogin(){
+    this.sharingData.handlerLoginEventEmitter.subscribe(({username,password})=>{
+      console.log(username + ' ' + password);
+
+      this.authService.loginUser({username,password}).subscribe({
+        next: response =>{
+          const token = response.token;
+          console.log(token);
+          const payload = JSON.parse(atob(token.split(".")[1]));
+
+          const user = {username: payload.sub}
+          const login = {
+            user,
+            isAuth: true,
+            isAdmin : payload.isAdmin
+          }
+          this.authService.token = token;
+          this.authService.user = login;
+          this.router.navigate(['/users/page/0'])
+        },
+        error: error =>{
+          if(error.status==401){
+            Swal.fire('Error en el Login', error.error.message, 'error')
+          }else{
+            throw error;
+          }
+
+        }
+      })
+    })
   }
 
   findUserById() {
---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,44 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  private url: string = 'http://localhost:8080/login'
+
+  private _token: string | undefined;
+  private _user: any = {
+    isAuth: false,
+    isAdmin: false,
+    user: undefined
+  };
+
+
+  constructor(private http: HttpClient) { }
+
+  loginUser({ username, password }: any): Observable<any> {
+    return this.http.post<any>(this.url, { username, password });
+  }
+  set user(user: any) {
+    this._user = user;
+    sessionStorage.setItem('login', JSON.stringify(user))
+
+  }
+
+  get user() {
+    return this._user
+  }
+
+  set token(token: string) {
+    this._token = token;
+    sessionStorage.setItem('token', token);
+
+  }
+
+  get token() {
+    return this._token!;
+  }
+}
