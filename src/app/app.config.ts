1a1b4945b128786e06b474ff423bc690ebb5aa00
becario3 <becario3@rufi.mx>
2025-06-23 13:41:08 -0600

    User actions agregados

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,9 +4,9 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { tokenInterceptor } from './interceptors/token.interceptor';
+import { provideStore } from '@ngrx/store';
 
 export const appConfig: ApplicationConfig = {
   providers: [provideRouter(routes),
-  provideHttpClient(withInterceptors([tokenInterceptor]))
-  ]
+    provideHttpClient(withInterceptors([tokenInterceptor])), provideStore()]
 };
