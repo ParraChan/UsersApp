e02b48f4327f19699bc9e1543467122c977db98b
becario3 <becario3@rufi.mx>
2025-06-23 16:05:15 -0600

    arreflando nugs

--- src/app/components/user-app.component.ts
+++ src/app/components/user-app.component.ts
@@ -14,136 +14,103 @@ import { add, find, findAll, remove, setPaginator, update } from '../store/users
   standalone: true,
   imports: [RouterOutlet, NavbarComponent],
   templateUrl: './user-app.component.html',
-  styleUrls: ['./user-app.component.css'],
-
+  styleUrls: ['./user-app.component.css']
 })
 export class UserAppComponent implements OnInit {
 
   users: User[] = [];
-  paginator: any={};
+  paginator: any = {};
   user!: User;
 
   constructor(
     private store: Store<{users: any}>,
     private router: Router,
-    private sharingData: SharingDataService,
     private service: UserService,
+    private sharingData: SharingDataService,
     private authService: AuthService,
-    private route: ActivatedRoute  ) {
-      this.store.select('users').subscribe(state =>{
-        this.users = state.users;
-        this.paginator= state.paginator;
-        this.user= state.user;
-      })
-  }
-
-  pageUsersEvent(){
-    this.sharingData.pageUsersEventEmitter.subscribe(pageable => {
-      //this.users = pageable.users;
-      //this.paginator = pageable.paginator;
-      this.store.dispatch(findAll({users: pageable.users}))
-      this.store.dispatch(setPaginator({paginator: pageable}))
+    private route: ActivatedRoute) {
+    this.store.select('users').subscribe(state => {
+      this.users = state.users;
+      this.paginator = state.paginator;
+      this.user = {... state.user};
     })
   }
 
   ngOnInit(): void {
-   /* this.route.paramMap.subscribe(params =>{
-      const page = +(params.get('page')  || '0');
-      console.log(page)
-       this.service.findAllPageable(page).subscribe(pageable => this.users = pageable.content as User[]);
-    })*/
-    this.service.findAll().subscribe(users => this.users = users);
-    this.pageUsersEvent();
     this.addUser();
     this.removeUser();
     this.findUserById();
+    this.pageUsersEvent();
     this.handlerLogin();
-
   }
-  handlerLogin(){
-    this.sharingData.handlerLoginEventEmitter.subscribe(({username,password})=>{
+
+  handlerLogin() {
+    this.sharingData.handlerLoginEventEmitter.subscribe(({ username, password }) => {
       console.log(username + ' ' + password);
 
-      this.authService.loginUser({username,password}).subscribe({
-        next: response =>{
+      this.authService.loginUser({ username, password }).subscribe({
+        next: response => {
           const token = response.token;
           console.log(token);
           const payload = this.authService.getPayload(token);
 
-          const user = {username: payload.sub}
+          const user = { username: payload.sub };
           const login = {
             user,
             isAuth: true,
-            isAdmin : payload.isAdmin
-          }
+            isAdmin: payload.isAdmin
+          };
+          
           this.authService.token = token;
           this.authService.user = login;
-          this.router.navigate(['/users/page/0'])
+          this.router.navigate(['/users/page/0']);
         },
-        error: error =>{
-          if(error.status==401){
+        error: error => {
+          if (error.status == 401) {
             Swal.fire('Error en el Login', error.error.message, 'error')
-          }else{
+          } else {
             throw error;
           }
-
         }
       })
     })
   }
 
+  pageUsersEvent() {
+    this.sharingData.pageUsersEventEmitter.subscribe(pageable => {
+      // this.users = pageable.users;
+      // this.paginator = pageable.paginator;
+      this.store.dispatch(findAll({ users: pageable.users}))
+      this.store.dispatch(setPaginator({ paginator: pageable.paginator}))
+    });
+  }
+
   findUserById() {
     this.sharingData.findUserByIdEventEmitter.subscribe(id => {
-     // const user = this.users.find(user => user.id == id)
-      this.store.dispatch(find({id}));
+
+      // const user = this.users.find(user => user.id == id);
+      this.store.dispatch(find({ id }))
       this.sharingData.selectUserEventEmitter.emit(this.user);
     })
-
   }
 
   addUser() {
     this.sharingData.newUserEventEmitter.subscribe(user => {
       if (user.id > 0) {
-        this.service.update(user).subscribe({
-          next: (userUpdated) => {
-           // this.users = this.users.map(u => (u.id == userUpdated.id) ? { ...userUpdated } : u)
-            this.store.dispatch(update({userUpdated}));
-           Swal.fire({
+        this.service.update(user).subscribe(
+          {
+            next: (userUpdated) => {
+              // this.users = this.users.map(u => (u.id == userUpdated.id) ? { ...userUpdated } : u);
+              this.store.dispatch(update({ userUpdated }));
+
+              this.router.navigate(['/users'], {
+                state: {
+                  users: this.users,
+                  paginator: this.paginator
+               } });
+            
+              Swal.fire({
               title: "Actualizado",
-              text: "El usuario se ha guardado correctamente",
-              width: 600,
-              padding: "3em",
-              color: "#716add",
-              background: "#fff", backdrop: `
-                rgba(0,0,123,0.4)
-                url("assets/img/cat.gif")
-                left top
-                no-repeat
-              `
-            }); this.router.navigate(['/users'], { state:
-               { users: this.users ,paginator: this.paginator } });
-
-
-          },
-          error: (err) => {
-            if (err.status == 400) {
-              this.sharingData.errorsUserFormEventEmitter.emit(err.error);
-            }
-            //console.log(err.error)
-          }
-        }
-        );
-
-      } else {
-
-        this.service.create(user).subscribe({
-          next: (userNew) => {
-            console.log(userNew)
-            this.users = [... this.users, { ...userNew }];
-           // this.router.navigate(['/users'], { state: { users: this.users ,paginator: this.paginator} });
-            this.store.dispatch(add({userNew}))
-            Swal.fire({
-              title: "Creado",
               text: "El usuario se ha actualizado correctamente",
               width: 600,
               padding: "3em",
@@ -155,58 +122,82 @@ export class UserAppComponent implements OnInit {
                 no-repeat
               `
             });
+
+            },
+            error: (err) => {
+              // console.log(err.error)
+              if (err.status == 400) {
+                this.sharingData.errorsUserFormEventEmitter.emit(err.error);
+              }
+            }
+          })
+
+      } else {
+        this.service.create(user).subscribe( {
+          next: userNew =>  {
+          console.log(user)
+          // this.users = [... this.users, { ...userNew }];
+            this.store.dispatch(add({ userNew }));
+
+            this.router.navigate(['/users'], {
+              state: {
+                users: this.users,
+                paginator: this.paginator
+             } });
+            
+            Swal.fire({
+              title: "Creado nuevo usuario!",
+              text: "Usuario creado con exito!",
+              icon: "success"
+            });
           },
           error: (err) => {
-            console.log(err.status);
+            // console.log(err.error)
+            // console.log(err.status)
             if (err.status == 400) {
               this.sharingData.errorsUserFormEventEmitter.emit(err.error);
             }
-          }
-        });
 
+        }})
       }
-      //this.router.navigateByUrl('/', { skipLocationChange: true }).then(() => {
-
-      // });
-
 
     })
-
   }
+
   removeUser(): void {
     this.sharingData.idUserEventEmitter.subscribe(id => {
       Swal.fire({
-        title: "Estas seguro?",
-        text: "nohay marcha atras",
+        title: "Seguro que quiere eliminar?",
+        text: "Cuidado el usuario sera eliminado del sistema!",
         icon: "warning",
         showCancelButton: true,
         confirmButtonColor: "#3085d6",
         cancelButtonColor: "#d33",
-        confirmButtonText: "Borrar!"
+        confirmButtonText: "Si"
       }).then((result) => {
         if (result.isConfirmed) {
-          this.service.remove(id).subscribe(() => {
-            //this.users = this.users.filter(user =>user.id != id);
-            this.store.dispatch(remove({id}))
-            this.router.navigate(['/users/create'], { skipLocationChange: true }).then(() => {
-              this.router.navigate(['/users'], { state: { users: this.users,paginator: this.paginator
 
-                
+          this.service.remove(id).subscribe(() => {
+            // this.users = this.users.filter(user => user.id != id);
+            this.store.dispatch(remove({ id }));
+            this.router.navigate(['/users/create'], { skipLocationChange: true }).then(() => {
+              this.router.navigate(['/users'], {
+                state: {
+                  users: this.users,
+                  paginator: this.paginator
                } });
             });
+          })
 
-          });
 
           Swal.fire({
             title: "Eliminado!",
-            text: "Usuario eliminado",
+            text: "Usuario eliminado con exito.",
             icon: "success"
           });
         }
       });
-
-    })
-
+    });
   }
 
 }
--- src/app/store/users.reducer.ts
+++ src/app/store/users.reducer.ts
@@ -2,49 +2,43 @@ import { createReducer, on } from "@ngrx/store";
 import { User } from "../models/user";
 import { add, find, findAll, remove, setPaginator, update } from "./users.actions";
 
-const users: User[]=[];
-const user: User= new User();
-
+const users: User[] = [];
+const user: User = new User();
 export const usersReducer = createReducer(
     {
         users,
         paginator: {},
-        user,
+        user
     },
-    on(findAll,(state, { users }) =>({
-            users: [... users],
-            paginator: state.paginator,
-            user: state.user
-        
+    on(findAll, (state, { users }) => ({
+        users: [...users],
+        paginator: state.paginator,
+        user: state.user
     }
     )),
-    on(find,(state,{id})=>({
+    on(find, (state, { id }) => ({
         users: state.users,
         paginator: state.paginator,
-        user:state.users.find(user => user.id == id) || new User()
+        user: state.users.find(user => user.id == id) || new User()
     })),
-    on(setPaginator,(state,{paginator})=>({
+    on(setPaginator, (state, { paginator }) => ({
         users: state.users,
-        paginator: {... paginator},
+        paginator: { ...paginator },
         user: state.user
     })),
-    on(add,(state,{userNew})=>({
-        users: [... state.users, { ...userNew }],
+    on(add, (state, { userNew }) => ({
+        users: [...state.users, { ...userNew }],
         paginator: state.paginator,
         user: state.user
     })),
-
-     on(update,(state,{ userUpdated }) => ({
-    users: state.users.map(u => (u.id == userUpdated.id) ? { ...userUpdated } : u),
-    paginator: state.paginator,
-    user: state.user
+    on(update, (state, { userUpdated }) => ({
+        users: state.users.map(u => (u.id == userUpdated.id) ? { ...userUpdated } : u),
+        paginator: state.paginator,
+        user: state.user
     })),
-
-    on(remove,(state,{ id }) => ({
+    on(remove, (state, { id }) => ({
         users: state.users.filter(user => user.id != id),
         paginator: state.paginator,
         user: state.user
     }))
-
-);//final
-   
+);
